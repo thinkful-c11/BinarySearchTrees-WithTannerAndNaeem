1f5d2b681699a22c523634aa5b1eec895005cd97
tannaurus <contacttannergill@gmail.com>
2017-08-01 08:31:45 -0700

    Added the get method to the JS file.

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -25,4 +25,16 @@ class BinarySearchTree {
       }
     }
   }
+
+  get(key) {
+    if (this.key === key) {
+      return this.value;
+    } else if (this.key < key && this.left) {
+      return this.left.get(key);
+    } else if (this.key > key && this.right) {
+      return this.right.get(key);
+    } else {
+      throw new Error("Key Error");
+    }
+  }
 }
