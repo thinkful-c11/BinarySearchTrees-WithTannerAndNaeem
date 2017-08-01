d5c20c06b57d86f842c91768b7c38dbdf3ef0406
tannaurus <contacttannergill@gmail.com>
2017-08-01 08:25:00 -0700

    Added the insert method.

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,9 +1,28 @@
 class BinarySearchTree {
-    constructor(key = null, value = null, parent = null ){
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
-        this.left = null;
-        this.right = null;
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        this.left.insert(key, value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
     }
+  }
 }
